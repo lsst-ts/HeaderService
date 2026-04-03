36b3267d7563cf56565159d02657cfe5520e1ef0
Max Turri <mxturri@gmail.com>
2026-04-03 08:02:33 -0700

    Updated version to 3.5.11

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for edir in etc_dirs:
 
 # The main call
 setup(name='HeaderService',
-      version='3.5.10',
+      version='3.5.11',
       license="GPL",
       description="LSST Meta-data aggregator for FITS header service",
       author="LSST, Felipe Menanteau",
