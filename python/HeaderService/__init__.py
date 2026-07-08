0ee54f6155c8a49deccd20fbbf489ac1725e2b51
Max Turri <mxturri@gmail.com>
2026-07-08 15:19:02 -0700

    OSW-2674 Removed TEMP_SET and CCDTEMP from ATCamera and CCDTEMP from CCCamera and MTCamera

--- python/HeaderService/__init__.py
+++ python/HeaderService/__init__.py
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __author__ = "LSST/Felipe Menanteau"
-__version__ = "3.5.12"
+__version__ = "3.5.13"
 version = __version__
 
 from . import hutils
